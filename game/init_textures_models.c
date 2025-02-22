2f45a0157bb8df7bb43ead316f5b5d6f5cb5b5cc
cfrey65 <cfreyschool@gmail.com>
2025-02-22 17:20:41 -0500

    Moved

--- game/init_textures_models.c
+++ game/init_textures_models.c
@@ -36,3 +36,32 @@ void load_textures_and_models() {
 Model model = LoadModel("resources/models/obj/castle.obj");                 // Load model
 Texture2D texture = LoadTexture("resources/models/obj/castle_diffuse.png"); // Load model texture
 model.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = texture;*/
+
+//If you want to attach a texture to a model consult the castle example on Raylib
+void unload_models() {
+    UnloadModel(baseplate_v2);
+    UnloadModel(peteypie);
+}
+
+void unload_textures(){
+    //Unload PurDoom
+    UnloadTexture(epic_github_logo);
+    UnloadTexture(spider_demon_model);
+    UnloadTexture(purdue_pete);
+
+    //Unload Galaga
+    UnloadTexture(purdue_galaga_plane);
+    UnloadTexture(purdue_scorpion);
+    UnloadTexture(purdue_galaxian);
+    UnloadTexture(purdue_galaga);
+    UnloadTexture(purdue_butterfly);
+    UnloadTexture(purdue_bumblebee);
+    UnloadTexture(purdue_boss_galaga);
+    UnloadTexture(purdue_boss_galaga_laser);
+    UnloadTexture(purdue_bosconian);
+}
+
+void unload_textures_and_models() {
+    unload_textures();
+    unload_models();
+}
