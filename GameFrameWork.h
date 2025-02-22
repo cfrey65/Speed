6d209a9f12e8a672809c477311589dc360f29970
cfrey65 <cfreyschool@gmail.com>
2025-02-22 00:26:03 -0500

    Updated the gameframework file to have external variable.

--- GameFrameWork.h
+++ GameFrameWork.h
@@ -1,3 +1,8 @@
-extern typedef struct g {
-    void* (getEnviroment*)
+typedef struct g {
+    void* game;
+    void (*updateEnviroment) (void* game);
+    void* (*getEnv) (void* game);
+    void* (*pause) (void* game);
 } game;
+
+extern game mainGame;
