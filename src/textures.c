5f0a6f5cf464f1f9625cf8d5dc96142643ea7ecc
Cheqhr <afewell@purdue.edu>
2025-02-22 14:38:22 -0500

    Modified textures.

--- src/textures.c
+++ src/textures.c
@@ -1,27 +1,17 @@
-//#include <Speed.h>
-
-/*int main() {
-    Vector3 pos = {0, 0, 0};
-    Camera3D cam = {0};
-    cam.position = (Vector3){ 0.0f, 10.0f, 10.0f };
-    cam.target = (Vector3){ 0.0f, 0.0f, 0.0f };
-    InitWindow(800, 450, "test");
-    Model model = LoadModel("C:\\raylib-master\\examples\\models\\resources\\models\\obj\\cube.obj");
-    Texture2D texture = LoadTexture("C:\\raylib-master\\examples\\models\\resources\\models\\obj\\cube_diffuse.png"); 
-    model.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = texture;
-    while (!WindowShouldClose()) {
-        UpdateCamera(&cam, CAMERA_FIRST_PERSON);
-        ClearBackground(RAYWHITE);
-        BeginDrawing();
-        BeginMode3D(cam);
-        DrawModel(model, pos, 1.0f, RED);
-        DrawGrid(20, 10.0f);
-        EndMode3D();
-        DrawText("(c) Cube 3D model", 250, 780, 10, GRAY);
-        DrawFPS(10, 10);
-        EndDrawing();
-    }
-}*/
+/*******************************************************************************************
+*
+*   raylib [core] example - Mouse input
+*
+*   Example complexity rating: [★☆☆☆] 1/4
+*
+*   Example originally created with raylib 1.0, last time updated with raylib 5.5
+*
+*   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
+*   BSD-like license that allows static linking with closed source software
+*
+*   Copyright (c) 2014-2025 Ramon Santamaria (@raysan5)
+*
+********************************************************************************************/
 
 #include "raylib.h"
 
@@ -35,82 +25,44 @@ int main(void)
     const int screenWidth = 800;
     const int screenHeight = 450;
 
-    InitWindow(screenWidth, screenHeight, "raylib [models] example - models loading");
+    Color ballColor = WHITE;
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - mouse input");
+    Texture2D text = LoadTexture("Models\\spider_demon_model.png"); 
 
-    // Define the camera to look into our 3d world
-    Camera camera = { 0 };
-    camera.position = (Vector2){ 50.0f, 50.0f}; // Camera position
-    camera.target = (Vector2){ 0.0f, 10.0f};     // Camera looking at point
-    camera.up = (Vector2){ 0.0f, 1.0f};          // Camera up vector (rotation towards target)
-    camera.fovy = 45.0f;                                // Camera field-of-view Y
-    camera.projection = CAMERA_PERSPECTIVE;                   // Camera mode type
+    Vector2 ballPosition = { -100.0f, -100.0f };
+    int isCursorHidden = 0;
 
-    Model model = LoadModel("C:\\raylib-master\\examples\\models\\resources\\models\\obj\\cube.obj");                 // Load model
-    Texture2D texture = LoadTexture("C:\\raylib-master\\examples\\models\\resources\\models\\obj\\cube_diffuse.png"); // Load model texture
-    model.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = texture;            // Set map diffuse texture
-
-    Vector2 position = { 0.0f, 0.0f};                    // Set model position
-
-    BoundingBox bounds = GetMeshBoundingBox(model.meshes[0]);   // Set model bounds
-
-    // NOTE: bounds are calculated from the original size of the model,
-    // if model is scaled on drawing, bounds must be also scaled
-
-    bool selected = false;          // Selected object flag
-
-    DisableCursor();                // Limit cursor to relative movement inside the window
-
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
+               // Set our game to run at 60 frames-per-second
+    //---------------------------------------------------------------------------------------
 
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
         // Update
         //----------------------------------------------------------------------------------
-        UpdateCamera(&camera, CAMERA_FIRST_PERSON);
-
-        // Load new models/textures on drag&drop
-        if (IsFileDropped())
+        if (IsKeyPressed(KEY_H))
         {
-            FilePathList droppedFiles = LoadDroppedFiles();
-
-            if (droppedFiles.count == 1) // Only support one file dropped
+            if (isCursorHidden == 0)
             {
-                if (IsFileExtension(droppedFiles.paths[0], ".obj") ||
-                    IsFileExtension(droppedFiles.paths[0], ".gltf") ||
-                    IsFileExtension(droppedFiles.paths[0], ".glb") ||
-                    IsFileExtension(droppedFiles.paths[0], ".vox") ||
-                    IsFileExtension(droppedFiles.paths[0], ".iqm") ||
-                    IsFileExtension(droppedFiles.paths[0], ".m3d"))       // Model file formats supported
-                {
-                    UnloadModel(model);                         // Unload previous model
-                    model = LoadModel(droppedFiles.paths[0]);   // Load new model
-                    model.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = texture; // Set current map diffuse texture
-
-                    bounds = GetMeshBoundingBox(model.meshes[0]);
-
-                    // TODO: Move camera position from target enough distance to visualize model properly
-                }
-                else if (IsFileExtension(droppedFiles.paths[0], ".png"))  // Texture file formats supported
-                {
-                    // Unload current model texture and load new one
-                    UnloadTexture(texture);
-                    texture = LoadTexture(droppedFiles.paths[0]);
-                    model.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = texture;
-                }
+                HideCursor();
+                isCursorHidden = 1;
             }
-
-            UnloadDroppedFiles(droppedFiles);    // Unload filepaths from memory
+            else
+            {
+                ShowCursor();
+                isCursorHidden = 0;
+            }
         }
+        
+        ballPosition = GetMousePosition();
 
-        // Select model on mouse click
-        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
-        {
-            // Check collision between ray and box
-            if (GetRayCollisionBox(GetScreenToWorldRay(GetMousePosition(), camera), bounds).hit) selected = !selected;
-            else selected = false;
-        }
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) ballColor = MAROON;
+        else if (IsMouseButtonPressed(MOUSE_BUTTON_MIDDLE)) ballColor = LIME;
+        else if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT)) ballColor = DARKBLUE;
+        else if (IsMouseButtonPressed(MOUSE_BUTTON_SIDE)) ballColor = PURPLE;
+        else if (IsMouseButtonPressed(MOUSE_BUTTON_EXTRA)) ballColor = YELLOW;
+        else if (IsMouseButtonPressed(MOUSE_BUTTON_FORWARD)) ballColor = ORANGE;
+        else if (IsMouseButtonPressed(MOUSE_BUTTON_BACK)) ballColor = BEIGE;
         //----------------------------------------------------------------------------------
 
         // Draw
@@ -119,22 +71,12 @@ int main(void)
 
             ClearBackground(RAYWHITE);
 
-            BeginMode2D(camera);
+            DrawTextureV(text, ballPosition, ORANGE);
+            DrawText("move ball with mouse and click mouse button to change color", 10, 10, 20, DARKGRAY);
+            DrawText("Press 'H' to toggle cursor visibility", 10, 30, 20, DARKGRAY);
 
-                DrawModel(model, position, 1.0f, WHITE);        // Draw 3d model with texture
-
-                DrawGrid(20, 10.0f);         // Draw a grid
-
-                if (selected) DrawBoundingBox(bounds, GREEN);   // Draw selection box
-
-            EndMode2D();
-
-            DrawText("Drag & drop model to load mesh/texture.", 10, GetScreenHeight() - 20, 10, DARKGRAY);
-            if (selected) DrawText("MODEL SELECTED", GetScreenWidth() - 110, 10, 10, GREEN);
-
-            DrawText("(c) Castle 3D model by Alberto Cano", screenWidth - 200, screenHeight - 20, 10, GRAY);
-
-            DrawFPS(10, 10);
+            if (isCursorHidden == 1) DrawText("CURSOR HIDDEN", 20, 60, 20, RED);
+            else DrawText("CURSOR VISIBLE", 20, 60, 20, LIME);
 
         EndDrawing();
         //----------------------------------------------------------------------------------
@@ -142,10 +84,7 @@ int main(void)
 
     // De-Initialization
     //--------------------------------------------------------------------------------------
-    UnloadTexture(texture);     // Unload texture
-    UnloadModel(model);         // Unload model
-
-    CloseWindow();              // Close window and OpenGL context
+    CloseWindow();        // Close window and OpenGL context
     //--------------------------------------------------------------------------------------
 
     return 0;
