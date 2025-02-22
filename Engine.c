bd9396716da3d5af190e62b95c5908f18ea63c5b
cfrey65 <cfreyschool@gmail.com>
2025-02-22 00:20:44 -0500

    Fixed errors can now run a game file.

--- Engine.c
+++ Engine.c
@@ -1,42 +1,50 @@
 #include "Engine.h"
-#include "GameFrameWork.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
 
 typedef struct st{
-    double cap;
-    double size;
-    void** mem;
+    int cap;
+    int size;
+    void* mem;
     void (*remove) (void*);
-    double instSize;
+    int instSize;
 } stack;
 
 void push(void* inst, stack* stack) {
     if (stack->size+1 > stack->cap) {
-        *(stack->mem) = realloc(*(stack->mem), stack->cap*2)
+        stack->mem = realloc(stack->mem, stack->cap*2);
         stack->cap = stack->cap*2;
     }
-    stack->mem[instSize*size]
+    *((char*)stack->mem + stack->instSize*stack->size) = *((char*)(inst));
     stack->size += 1;
 }
 
 void* pop(stack* stack) {
     stack->size -= 1;
-    void* inst = stack->instSize*stack->size
-    stack->remove(stack->mem+(stack->instSize*stack->size))
-    free(stack->instSize*stack->size)
+    void* inst = (char*)stack->mem + stack->instSize*stack->size;
+    stack->remove(stack->mem+(stack->instSize*stack->size));
+    free((char*)stack->mem + stack->instSize*stack->size);
     return inst;
 }
 
-int main(char** args) {
-    char* cmd = "gcc " + args + "-o game";
-    int err = system(cmd);
-    if (err != 0) {
+int main(int argc, char** args) {
+    char* cmd = malloc(4 + strlen(args[1]) + 9);
+    if (!cmd) {
+        printf("Could not allocate memory!");
+        return 1;
+    }
+    memcpy(cmd, "gcc ", 4);
+    memcpy(cmd+4, args[1], strlen(args[1]));
+    memcpy(cmd+4+strlen(args[1]), " -o game", 8);
+    cmd[4+strlen(args[1])+9] = '\0';
+    if (system(cmd) != 0) {
         printf("Game file compilation error!");
         return 1;
     }
-    system("./game");
+    free(cmd);
+    cmd = NULL;
+    system("game.exe");
     return 0;
 }
