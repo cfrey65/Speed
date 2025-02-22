4b2e9985e7c1f831501e0f63f8e487aa17dcf685
J.D. Jackson <joshua.d.jackson45@gmail.com>
2025-02-22 03:00:30 -0500

    Modified framework

--- GameFrameWork.h
+++ GameFrameWork.h
@@ -1,12 +1,13 @@
+#pragma once
+
 typedef struct gaming {
-    void* game;
-    size_t envSize = 1;
-    void (*updateEnviroment) (void* game, char c);
-    void* (*getEnv) (void* game);
-    void* (*pause) (void* game);
-    void (*deleteEnv) (void* env);
-    char* interrupts;
-    int numInt;
+    void* game; //self explanatory
+    size_t envSize; //do not touch (maybe)
+    void (*updateEnviroment) (void* game, char c); //game is the game state
+    //and char is for the input action
+    void* (*getEnv) (void* game); //check if things need to be updated
+    void* (*pause) (void* game); //checks for pause state
+    void (*deleteEnv) (void* env); //frees enviroment
 } g;
 
 global g* mainGame;
